dae42fd42c4c24427a9ff411106dd50718bdeab6
charging station <mydreamyourpresent@yahoo.com>
2026-07-07 13:55:07 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,6 @@
+window.QUESTIONS = [
+  "Can you describe your current role in the Emergency Department?",
+  "What are the most time-consuming tasks during your shift?",
+  "Where do you usually experience delays in the patient flow?",
+  "Can you describe the main handoffs between your role and other staff members?"
+];
